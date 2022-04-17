c95efdd7d25368883baa6bdc34bfb12a529bc113
OzanKaracanta <101042596+OzanKaracanta@users.noreply.github.com>
2022-04-17 20:34:11 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      spacing: {
+        '128': '32rem',
+      },
+      backgroundImage: {
+        'hero-pattern': "url('email-bg.png')",
+      },
+      fontFamily: {
+        'montserrat': ['Montserrat'],
+        'lato': ['Lato'],
+        'garamond': ['Garamond']
+    }
+    },
+  },
+  plugins: [],
+}
